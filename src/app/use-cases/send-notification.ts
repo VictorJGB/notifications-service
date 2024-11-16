da87ed7de8d737564f0f3f8ee62ad593989499e4
VictorJGB <victorgb.dev@gmail.com>
2024-11-16 15:39:16 -0300

    feat: Creating the send notification use case

---
+++ src/app/use-cases/send-notification.ts
@@ -0,0 +1,26 @@
+import { Content } from "../entities/content"
+import { Notification } from "../entities/notification"
+
+interface sendNotificationRequest {
+  recipientId: string
+  content: string
+  category: string
+}
+
+interface sendNotificationResponse {
+  notification: Notification
+}
+
+export class SendNotification {
+  async execute(request: sendNotificationRequest): Promise<sendNotificationResponse> {
+    const { recipientId, category, content } = request
+
+    const notification = new Notification({
+      recipientId,
+      content: new Content(content),
+      category
+    })
+
+    return { notification }
+  }
+}
